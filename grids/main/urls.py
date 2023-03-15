c9fe8d018c60d81a363721e3c51ab2a9b74b41fc
Drebedenb <danila.kuzya2001@gmail.com>
2023-03-15 14:55:47 +0300

    Base template and other templates downloaded and working

--- grids/main/urls.py
+++ grids/main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index),
+    path('<str:page_name>/', render_page),  # page_name is the name of the page and file
 ]
--- grids/main/views.py
+++ grids/main/views.py
@@ -6,5 +6,9 @@ def index(request):
     return render(request, 'main/index.html')
 
 
+def render_page(request, page_name):
+    return render(request, 'main/' + page_name + '.html')
+
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("Page NOT found")
