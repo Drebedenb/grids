0eeec5e603ceddb352c10b0c4118e87978a1d194
Drebedenb <danila.kuzya2001@gmail.com>
2023-03-21 10:30:30 +0300

    Change settings and gunicorn file

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /home/www/code/grids/env/bin/activate
-source /home/www/code/grids/env/bin/postactivate
 exec gunicorn  -c "/home/www/code/grids/grids/gunicorn_config.py" grids.wsgi
