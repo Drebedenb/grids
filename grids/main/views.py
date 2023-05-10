0262c5305e934be2260a5001907125fec56b9c9e
Drebedenb <danila.kuzya2001@gmail.com>
2023-05-10 08:57:46 +0300

    To prod

--- grids/main/views.py
+++ grids/main/views.py
@@ -5,19 +5,19 @@ from django.db.models import Min, Max
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from django.core.cache import cache
+from django.core.cache import cache
 from urllib.parse import urlencode
 import collections.abc
+#
+# class MockDjangoRedis:
+#     def get(self, arg):
+#         return None
+#
+#     def set(arg, bla, ble, blu):
+#         return arg
+#
+# cache = MockDjangoRedis()
 
-class MockDjangoRedis:
-    def get(self, arg):
-        return None
-
-    def set(arg, bla, ble, blu):
-        return arg
-
-
-cache = MockDjangoRedis()
 
 from .models import PriceWinguardMain, PriceWinguardFiles, PriceWinguardSketch
 
