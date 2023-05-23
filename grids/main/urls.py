fae2d13ff500ad0cfca4b394a2307a3440c03767
Drebedenb <danila.kuzya2001@gmail.com>
2023-05-23 11:41:33 +0300

    Change urls to transliteration form

--- grids/main/urls.py
+++ grids/main/urls.py
@@ -4,12 +4,12 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('<str:category_name>', catalog_category, name='catalog'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:category>-<int:file_number>', product, name='product'),
-    path('projects/', projects, name='projects'),
-    path('reviews/', reviews, name='reviews'),
-    path('compare/', compare, name="compare"),
-    path('favorite/', favorite, name="favorite"),
+    path('kontakty/', contacts, name='contacts'),
+    path('reshetka-na-okno/<int:category>-<int:file_number>', product, name='product'),
+    path('nashi-raboty/', projects, name='projects'),
+    path('otzyvy-klientov/', reviews, name='reviews'),
+    path('sravnenie-reshetki-na-okna/', compare, name="compare"),
+    path('lyubimye-reshetki-na-okna/', favorite, name="favorite"),
     path('privacy/', privacy, name="privacy"),
-    path('акции/', sales, name="sales")
+    path('akcii/', sales, name="sales")
 ]
