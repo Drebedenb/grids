09148aebceba02f4ea1c1b6feca3eadd62ac0efa
Drebedenb <danila.kuzya2001@gmail.com>
2023-05-23 11:54:54 +0300

    Change urls to Russian language all

--- grids/main/urls.py
+++ grids/main/urls.py
@@ -4,12 +4,12 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('<str:category_name>', catalog_category, name='catalog'),
-    path('kontakty/', contacts, name='contacts'),
-    path('reshetka-na-okno/<int:category>-<int:file_number>', product, name='product'),
-    path('nashi-raboty/', projects, name='projects'),
-    path('otzyvy-klientov/', reviews, name='reviews'),
-    path('sravnenie-reshetki-na-okna/', compare, name="compare"),
-    path('lyubimye-reshetki-na-okna/', favorite, name="favorite"),
-    path('privacy/', privacy, name="privacy"),
-    path('akcii/', sales, name="sales")
+    path('контакты/', contacts, name='contacts'),
+    path('решетка-на-окно/<int:category>-<int:file_number>', product, name='product'),
+    path('наши-работы/', projects, name='projects'),
+    path('отзывы-клиентов/', reviews, name='reviews'),
+    path('сравнение-решеток-на-окна/', compare, name="compare"),
+    path('избранные-решетки-на-окна/', favorite, name="favorite"),
+    path('политика-конфиденциальности/', privacy, name="privacy"),
+    path('акции/', sales, name="sales")
 ]
